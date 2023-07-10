0f0b1b608585222fc30633a8ca85824783f53e54
Mjtala <62020259+Mjtala@users.noreply.github.com>
2023-07-09 21:28:11 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use(async (ctx, next) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(4000, "0.0.0.0", () => {
   console.log('server is running at http://localhost:3000');
 });
 
